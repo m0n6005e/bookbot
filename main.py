83eb66b133f0478206aaed19428aa707814ae556
m0n6005e <m0n6005e@gmail.com>
2025-04-30 13:30:47 -0600

    Final bookbot

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from stats import num_words
+from stats import num_characters
+from stats import dictonary_sort
+from stats import output_message
+import sys
 
 def main():
-    book_text = get_book_text("/home/m0n6005e/bookbot/books/frankenstein.txt")
-    num_words(book_text)
+    #get book from somewhere and put it in book
+    if len(sys.argv) > 1:
+        book = sys.argv[1]          #"/home/m0n6005e/bookbot/books/frankenstein.txt"
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    book_text = get_book_text(book)
+    word_count = num_words(book_text)  
+    character_count = num_characters(book_text) 
+    sorted_dictonary = dictonary_sort(character_count)
+    output_message(word_count, sorted_dictonary, book)
+   
 
 def get_book_text(bookPath):
-    with open(bookPath) as f:        
+    with open(bookPath, "r", encoding="latin-1") as f:   
         return f.read()
 
 
--- stats.py
+++ stats.py
@@ -1,5 +1,42 @@
 def num_words(book_text):
     words = book_text.split()
-    word_count = len(words)
-    print(f"{word_count} words found in the document")
+    return len(words) 
+    
+
+def num_characters(book_text):
+    text = book_text.lower()
+    letter_dict = {}
+    for char in text:
+        if char in letter_dict:
+            letter_dict[char] += 1
+        else:
+            letter_dict[char] = 1
+
+    return letter_dict
+
+def dictonary_sort(charactor_dictonary):
+    char_count_list = []
+    for key, value in charactor_dictonary.items():
+        if key.isalpha():
+            dict1 = {} 
+            dict1["char"] = key
+            dict1["num"] = value
+            char_count_list.append(dict1)
+    char_count_list.sort(reverse=True, key=sort_on)          
+    return char_count_list
+
+def output_message(word_count, sorted_list, book):
+    print("============ BOOKBOT ============") 
+    print(f"Analyzing book found at {book}")
+    print("----------- Word Count -----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count ---------")
+    for item in sorted_list:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END =============")
+
+def sort_on(charactor_dictonary):
+    return charactor_dictonary["num"]
+
+    
     
